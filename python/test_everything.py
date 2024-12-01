6fc9d764db3278aef2044fa9a189d5b0835f88b9
RuslanUC <dev_ruslan_uc@protonmail.com>
2024-12-01 15:48:11 +0200

    create python module directory

--- python/test_everything.py
+++ python/test_everything.py
@@ -6,7 +6,7 @@ import pytest
 print(f"\nPid: {os.getpid()}")
 input("Press enter to continue...")
 
-import _pyutl
+from pyutl import def_pool as pool, TLObject
 
 
 SKIP_TESTS = 0
@@ -14,13 +14,12 @@ SKIP_TESTS = 0
 
 @pytest.mark.skipif(SKIP_TESTS >= 1, reason="")
 def test_parse() -> None:
-    pool = _pyutl.def_pool
     assert pool
 
     cls = pool.parse("inputGeoPoint#48222faf flags:# lat:double long:double accuracy_radius:flags.0?int = InputGeoPoint;")
     assert cls
-    assert issubclass(cls, _pyutl.TLObject)
-    assert cls is not _pyutl.TLObject
+    assert issubclass(cls, TLObject)
+    assert cls is not TLObject
 
     assert pool.has_type("InputGeoPoint")
     assert not pool.has_type("InputGeoPointa")
@@ -33,7 +32,6 @@ def test_parse() -> None:
 
 @pytest.mark.skipif(SKIP_TESTS >= 2, reason="")
 def test_create_object() -> None:
-    pool = _pyutl.def_pool
     assert pool
 
     cls = pool.parse("inputGeoPoint#48222faf flags:# lat:double long:double accuracy_radius:flags.0?int = InputGeoPoint;")
@@ -52,7 +50,6 @@ def test_create_object() -> None:
 
 @pytest.mark.skipif(SKIP_TESTS >= 3, reason="")
 def test_encode_object() -> None:
-    pool = _pyutl.def_pool
     assert pool
 
     cls = pool.parse("inputGeoPoint#48222faf flags:# lat:double long:double accuracy_radius:flags.0?int = InputGeoPoint;")
@@ -67,7 +64,6 @@ def test_encode_object() -> None:
 
 @pytest.mark.skipif(SKIP_TESTS >= 4, reason="")
 def test_decode_object() -> None:
-    pool = _pyutl.def_pool
     assert pool
 
     cls = pool.parse("inputGeoPoint#48222faf flags:# lat:double long:double accuracy_radius:flags.0?int = InputGeoPoint;")
@@ -83,12 +79,12 @@ def test_decode_object() -> None:
     assert obj.long == 24.42
     assert obj.accuracy_radius == 456123
 
-    obj = _pyutl.TLObject.read_bytes(bytes([0xaf, 0x2f, 0x22, 0x48, 0x0, 0x0, 0x0, 0x0, 0x1f, 0x85, 0xeb, 0x51, 0xb8, 0x1e, 0x45, 0x40, 0xec, 0x51, 0xb8, 0x1e, 0x85, 0x6b, 0x38, 0x40]))
+    obj = TLObject.read_bytes(bytes([0xaf, 0x2f, 0x22, 0x48, 0x0, 0x0, 0x0, 0x0, 0x1f, 0x85, 0xeb, 0x51, 0xb8, 0x1e, 0x45, 0x40, 0xec, 0x51, 0xb8, 0x1e, 0x85, 0x6b, 0x38, 0x40]))
     assert obj.lat == 42.24
     assert obj.long == 24.42
     assert obj.accuracy_radius is None
 
-    obj = _pyutl.TLObject.read_bytes(bytes([0xaf, 0x2f, 0x22, 0x48, 0x01, 0x0, 0x0, 0x0, 0x1f, 0x85, 0xeb, 0x51, 0xb8, 0x1e, 0x45, 0x40, 0xec, 0x51, 0xb8, 0x1e, 0x85, 0x6b, 0x38, 0x40, 0xbb, 0xf5, 0x06, 0x00]))
+    obj = TLObject.read_bytes(bytes([0xaf, 0x2f, 0x22, 0x48, 0x01, 0x0, 0x0, 0x0, 0x1f, 0x85, 0xeb, 0x51, 0xb8, 0x1e, 0x45, 0x40, 0xec, 0x51, 0xb8, 0x1e, 0x85, 0x6b, 0x38, 0x40, 0xbb, 0xf5, 0x06, 0x00]))
     assert obj.lat == 42.24
     assert obj.long == 24.42
     assert obj.accuracy_radius == 456123
@@ -96,7 +92,6 @@ def test_decode_object() -> None:
 
 @pytest.mark.skipif(SKIP_TESTS >= 5, reason="")
 def test_nested_message() -> None:
-    pool = _pyutl.def_pool
     assert pool
 
     inputPeerUser = pool.parse("inputPeerUser#dde8a54c user_id:long access_hash:long = InputPeer;")
@@ -120,7 +115,7 @@ def test_nested_message() -> None:
     assert obj2.peer.user_id == obj.peer.user_id
     assert obj2.peer.access_hash == obj.peer.access_hash
 
-    obj2 = _pyutl.TLObject.read_bytes(serialized)
+    obj2 = TLObject.read_bytes(serialized)
     assert obj2.msg_id == obj.msg_id
     assert obj2.user_id == obj.user_id
     assert obj2.peer.user_id == obj.peer.user_id
@@ -129,7 +124,6 @@ def test_nested_message() -> None:
 
 @pytest.mark.skipif(SKIP_TESTS >= 6, reason="")
 def test_equals() -> None:
-    pool = _pyutl.def_pool
     assert pool
 
     inputPeerUser = pool.parse("inputPeerUser#dde8a54c user_id:long access_hash:long = InputPeer;")
@@ -144,7 +138,7 @@ def test_equals() -> None:
     assert obj.peer.user_id == 123
     assert obj.peer.access_hash == 456
 
-    obj2 = _pyutl.TLObject.read_bytes(obj.write())
+    obj2 = TLObject.read_bytes(obj.write())
     assert obj2.msg_id == obj.msg_id
     assert obj2.user_id == obj.user_id
     assert obj2.peer.user_id == obj.peer.user_id
@@ -162,7 +156,6 @@ def test_equals() -> None:
 
 @pytest.mark.skipif(SKIP_TESTS >= 7, reason="")
 def test_vector() -> None:
-    pool = _pyutl.def_pool
     assert pool
 
     photoSizeProgressive = pool.parse("photoSizeProgressive#fa3efb95 type:string w:int h:int sizes:Vector<int> = PhotoSize;")
